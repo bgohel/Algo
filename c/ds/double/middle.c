34f6af08bfbc42a1dfd2c2664c39ef205ffd13de
bgohel <bhargavp1993@gmail.com>
2017-02-03 14:44:32 +0530

    sort

--- c/ds/double/middle.c
+++ c/ds/double/middle.c
@@ -7,28 +7,24 @@ struct st
 	struct st *next;
 };
 typedef struct st DLL;
-void add_beg(void);
-void add_end(void);
+void add_middle(void);
 void print(void);
 DLL *headptr;//by default 0
 main()
 {
-char ch;
+	char ch;
 
-do
-{
-//add_beg();
-add_end();
-printf("do you Want another node........(y/Y)?");
-scanf(" %c",&ch);
+	do
+	{
+		add_middle();
+		printf("do you Want another node........(y/Y)?\n");
+		scanf(" %c",&ch);
 
-}while(ch=='y'||ch=='Y');
+	}while(ch=='y'||ch=='Y');
 
-//	add_beg();
-//	add_beg();
-//	add_beg();
-
-	print();
+	//	add_beg();
+	//	add_beg();
+	//	add_beg();
 	printf("__________________________________________\n");
 	print();
 
@@ -36,29 +32,53 @@ scanf(" %c",&ch);
 
 void add_middle(void)
 {
-        DLL *temp,*temp1;
+	DLL *temp,*temp1;
 
-        temp=malloc(sizeof(DLL));
-        ///////////////////////////////////////////////////////////
-        printf("Enter the num...\n");
-        scanf("%d",&temp->n);
+	temp=malloc(sizeof(DLL));
+	///////////////////////////////////////////////////////////
+	printf("Enter the num...\n");
+	scanf("%d",&temp->n);
 
-        temp->next=0;
-        temp->prev=0;
-        ///////////////////////////////////////////////////////////
-
-
+	temp->next=0;
+	temp->prev=0;
+	///////////////////////////////////////////////////////////
 
 
+	if(headptr==0)
+	{
+		headptr=temp;
+	}
+	else if((temp->n)<(headptr->n))
+	{
+		temp->next=headptr;
+		headptr->prev=temp;
+		headptr=temp;
+	}
+	else
+	{
+		temp1=headptr;
+		while(temp1)
+		{
+			if(temp1->next==0)
+			{
+				temp1->next=temp;
+				temp->prev=temp1;
+				break;
+			}
+			if((temp1->next->n)>(temp->n))
+			{
+				temp->prev=temp1;
+				temp->next=temp1->next;;
+				temp1->next->prev=temp;
+				temp1->next=temp;
+				break;
+			}
+
+			temp1=temp1->next;
+		}
 
 
-
-
-
-
-
-
-
+	}
 }
 
 
@@ -67,7 +87,7 @@ void add_middle(void)
 void print(void)
 {
 	DLL *ptr;
-ptr=headptr;
+	ptr=headptr;
 	while(ptr)
 	{
 
